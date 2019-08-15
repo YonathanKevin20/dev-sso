871dd9ab1a89052d4f17d74f45b03cb6122f24b1
Kevin <yonathan.kevin20@gmail.com>
2019-08-15 17:21:11 +0700

    show attributes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+$attr = array(
+    'uid' => Cas::getAttribute('uid'),
+    'displayName' => Cas::getAttribute('displayName'),
+    'givenName' => Cas::getAttribute('givenName'),
+    'cn' => Cas::getAttribute('cn'),
+    'sn' => Cas::getAttribute('sn')
+);
+@endphp
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,13 +17,15 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in! Hello {{ Cas::getAttribute('cn') }} !
+                    <div class="alert alert-success" role="alert">
+                        You are logged in! Hello {{ Cas::getAttribute('cn') }}!
+                    </div>
+                    Attributes:
+                    <ul>
+                        @foreach($attr as $key => $value)
+                        <li>{{ $key }}: {{ $value }}</li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
